0601e912e4d7335bf5dfa41f34a3fc5b09ffffaf
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-23 11:14:09 -0600

    Personalized widget - cardswiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/widget/card_swiper_widget.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -21,7 +20,7 @@ class HomePage extends StatelessWidget {
       body: SafeArea( //para respetar el notch de los celulares mas nuevos
           child: Column(
             children: <Widget>[
-              _swiperTarjetas()
+              _swiperTarjetas(),
             ],
           ),
       ),
@@ -30,21 +29,8 @@ class HomePage extends StatelessWidget {
 
  Widget _swiperTarjetas() {
 
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 150.0,
-      child: Swiper(
-        layout: SwiperLayout.STACK,
-        itemWidth: 200.0,
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
-
-      ),
+    return CardSwiper (
+      peliculas: [1,2,3,4,5],
     );
  }
 }
