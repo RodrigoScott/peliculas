8457a6c2fa8dcd3ec110006bbe5628eea94dd641
Rodrigo Scott <scott_rona1@hotmail.com>
2019-12-30 15:11:53 -0600

    detail screen

--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -10,9 +10,24 @@ class MovieDetail extends StatelessWidget {
     final Movie movie = ModalRoute.of(context).settings.arguments;
     
     return Scaffold(
+      backgroundColor: Colors.black,
       body: CustomScrollView(
         slivers: <Widget>[//esto es parecido al child
           _createAppBar(movie),
+          SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  SizedBox(height: 20.0),
+                  _titlePoster(movie),
+                  _description(movie),
+                  _description(movie),
+                  _description(movie),
+                  _description(movie),
+
+                ]
+
+              ),
+          )
 
         ],
       )
@@ -33,7 +48,7 @@ class MovieDetail extends StatelessWidget {
         centerTitle: true,
         title: Text(
           movie.title,
-          style: TextStyle(color: Colors.white, fontSize: 12.0),
+          style: TextStyle(color: Colors.white, fontSize: 20.0),
         ),
         background:
         FadeInImage(
@@ -50,4 +65,50 @@ class MovieDetail extends StatelessWidget {
     );
 
  }
+
+Widget  _titlePoster(Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(image: NetworkImage(movie.getPosterImg()), height: 150.0,
+            ),
+          ),
+          SizedBox(width: 15.0),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(movie.title, style: TextStyle(color: Colors.white, fontSize: 13.0, ), overflow: TextOverflow.ellipsis),
+                SizedBox(height: 10.0,),
+                Text(movie.originalTitle, style: TextStyle(color: Colors.white, fontSize: 10.0, ), overflow: TextOverflow.ellipsis),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border, color: Colors.white, size: 15.0,),
+                    Text(movie.voteAverage.toString(), style: TextStyle(color: Colors.white, fontSize: 15.0, ), overflow: TextOverflow.ellipsis),
+                  ],
+                )
+
+
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
+
+}
+
+Widget  _description(Movie movie) {
+    
+    return Container(
+      padding: EdgeInsets.all(20.0),
+
+      child: Text(movie.overview, style: TextStyle(color: Colors.white, fontSize: 15.0, ), textAlign: TextAlign.justify,),
+
+    );
+}
 }
